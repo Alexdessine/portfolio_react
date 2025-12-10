ba11c25936336b082853700db4834dc51dfe72c4
Alexdessine <alexandre@bourlier.email>
2025-12-10 21:35:44 +0100

    Faire un test de vérification

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Hello, World!"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
