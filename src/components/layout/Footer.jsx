3924a0a2a6cc0d1e36b8de879bc9a4e8b509230e
Alexdessine <alexandre@bourlier.email>
2025-12-11 12:00:48 +0100

    Mise en place du footer avec les liens vers les pages

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
 function Footer() {
@@ -7,17 +8,37 @@ function Footer() {
             <section>
                 <p>John Doe</p>
                 <address>
-                    <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MTIwOC4wIKXMDSoASAFQAw%3D%3D">40 rue Laure Diebold</a>
-                    <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MTIwOC4wIKXMDSoASAFQAw%3D%3D">69009 Lyon, France</a>
-                    <a href="tel:+33606060606">06.06.06.06.06</a>
-                    <a href="mailto:example@mail.com">example@mail.com</a>
+                    <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MTIwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">40 rue Laure Diebold</a>
+                    <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.778662,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI1MTIwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">69009 Lyon, France</a>
+                    <a href="tel:+33606060606" target="_blank">06.06.06.06.06</a>
+                    <a href="mailto:example@mail.com" target="_blank">example@mail.com</a>
                 </address>
+                <div className="social-link">
+                    <a href="" target="_blank"><i class="bi bi-github"></i></a>
+                    <a href="" target="_blank"><i class="bi bi-twitter-x"></i></a>
+                    <a href="" target="_blank"><i class="bi bi-linkedin"></i></a>
+                </div>
             </section>
             <section>
-                <div ><h2>Ici le footer</h2></div>
+                <p>Liens utiles</p>
+                <ul>
+                    <li><NavLink to="/">Accueil</NavLink></li>
+                    <li><NavLink to="/services">Services</NavLink></li>
+                    <li><NavLink to="/realisations">Portfolio</NavLink></li>
+                    <li><NavLink to="/contact">Me contacter</NavLink></li>
+                    <li><NavLink to="/mentions-legales">Mentions légales</NavLink></li>
+                </ul>
             </section>
             <section>
-                <div ><h2>Ici le footer</h2></div>
+                <p>Mes dernières réalisations</p>
+                <ul>
+                    <li><NavLink to="/realisations">FreshFood</NavLink></li>
+                    <li><NavLink to="/realisations">Restaurant Akira</NavLink></li>
+                    <li><NavLink to="/realisations">Espace bien-être</NavLink></li>
+                    <li><NavLink to="/realisations">SEO</NavLink></li>
+                    <li><NavLink to="/realisations">Création d'une API</NavLink></li>
+                    <li><NavLink to="/realisations">Maquette d'un site web</NavLink></li>
+                </ul>
             </section>
         </footer>
     )
