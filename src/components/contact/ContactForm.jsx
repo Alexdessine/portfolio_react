57a6ab73adac7591b28d4dd6cdd7ea48a2ad96bd
Alexdessine <alexandre@bourlier.email>
2025-12-12 11:44:41 +0100

    Ajout de la fonctionnalité alert sur la soumission du formulaire

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import SectionCard from "../ui/SectionCard";
 
 function ContactForm() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const subject = form.subject.value;
+        const message = form.message.value;
+
+        alert(
+            `Nom : ${name}\n` +
+            `Email : ${email}\n` +
+            `Téléphone : ${phone}\n` +
+            `Sujet : ${subject}\n` +
+            `Message : ${message}`
+        );
+    };
     return (
         <SectionCard title="Formulaire de contact">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <input 
                         type="text"
+                        name="name"
                         className="form-control"
                         placeholder="Votre nom"
                         required
@@ -16,6 +37,7 @@ function ContactForm() {
                 <div className="mb-3">
                     <input 
                         type="email"
+                        name="email"
                         className="form-control"
                         placeholder="Votre adresse email"
                         required
@@ -24,6 +46,7 @@ function ContactForm() {
                 <div className="mb-3">
                     <input 
                         type="tel"
+                        name="phone"
                         className="form-control"
                         placeholder="Votre numéro de téléphone"
                         required
@@ -32,6 +55,7 @@ function ContactForm() {
                 <div className="mb-3">
                     <input 
                         type="text"
+                        name="subject"
                         className="form-control"
                         placeholder="Sujet"
                         required
@@ -39,13 +63,14 @@ function ContactForm() {
                 </div>
                 <div className="mb-3">
                     <textarea
+                        name="message"
                         className="form-control"
                         rows="7"
                         placeholder="Votre message"
                         required
                     ></textarea>
                 </div>
-                <button type="submit" className="btn btn-primary text-center">
+                <button type="submit" className="btn btn-primary">
                     Envoyer
                 </button>
             </form>
